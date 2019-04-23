8a15f87e2e74532c96be3384929b70725f0f6f03
archi-tektur <kontakt@archi-tektur.pl>
2019-04-24 00:04:23 +0200

    Created mastered pagination with KnpPaginationBundle

--- src/Controller/GUI/RouteController.php
+++ src/Controller/GUI/RouteController.php
@@ -6,6 +6,7 @@ namespace App\Controller\GUI;
 use App\Entity\Infrastructure\Route;
 use App\Entity\Infrastructure\Station;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route as GET;
@@ -20,6 +21,8 @@ class RouteController extends AbstractController
     /** @var EntityManagerInterface */
     protected $entityManager;
 
+    private const ELEMENTS_ON_PAGE = 8;
+
     /**
      * Assigns data from arguments as class fields
      *
@@ -31,6 +34,8 @@ class RouteController extends AbstractController
     }
 
     /**
+     * Renders route details screen
+     *
      * @GET("/route/{kbs}", requirements={"kbs"="\d+"}, name="gui__route_details")
      * @param Route|null $route
      * @return Response
@@ -58,14 +63,26 @@ class RouteController extends AbstractController
     }
 
     /**
-     * @GET("/route/list", name="gui__route_list")
+     * Shows screen for route choose
+     *
+     * @GET("/route/list/{page}", name="gui__route_list")
+     * @param PaginatorInterface $paginator
+     * @param int                $page
+     * @return Response
      */
-    public function routeChooseScreen(): Response
+    public function routeChooseScreen(PaginatorInterface $paginator, int $page = 1): Response
     {
+        $dql = 'SELECT a FROM App\Entity\Infrastructure\Route a ORDER BY a.id ASC';
+        $query = $this->entityManager->createQuery($dql);
 
-        $routes = $this->entityManager->getRepository(Route::class)->findAll();
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $page,
+            self::ELEMENTS_ON_PAGE
+        );
+
         return $this->render('route/route_choose.html.twig', [
-            'routes' => $routes,
+            'pagination' => $pagination,
         ]);
     }
 }
