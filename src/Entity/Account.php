b1b4d60d578e8399b47d875251cb70e4fa2c1029
archi-tektur <kontakt@archi-tektur.pl>
2019-04-26 18:57:09 +0200

    Created first login screen, based on FOSUserBundle

---
+++ src/Entity/Account.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="account")
+ */
+class Account extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
